eb398afbba372e94d2b8af75324a470316c7718a
Anne-Berit Murberg <anne-beritm@hotmail.com>
2021-07-18 13:02:01 +0200

    revorked launches

---
+++ js/rockets.js
@@ -0,0 +1,28 @@
+const rocketsUrl = "https://api.spacexdata.com/v4/rockets";
+
+const rocketsContainer = document.querySelector(".rockets-container");
+
+async function fetchRockets() {
+    try {
+        const responseRockets = await fetch(rocketsUrl);
+        const rockets = await responseRockets.json(); 
+
+        console.log(rockets);
+
+        rocketsContainer.innerHTML = "";
+
+        for (let i = 0; i < rockets.length; i++) {
+            console.log(rockets[i].engines.propellant_2)
+            rocketsContainer.innerHTML += `
+            <a href="rocket_detail.html?id=${rockets[i].id}">
+            <div class="launches">
+            <img alt="Picture of ${rockets[i].name}" referrerpolicy="no-referrer" class="images" src="${rockets[i].flickr_images}">
+            <h2>${rockets[i].name}</h2>
+            </div>`;
+        }
+    } catch (error) {
+        console.log(error);
+        rocketsContainer.innerHTML = message("Sorry, something went wrong &#128533", "error");
+    }
+}
+fetchRockets();
